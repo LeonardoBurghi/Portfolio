e370d4cdf1eb1b45f287ab55f762dd769022410d
LeonardoBurghi <79922448+LeonardoBurghi@users.noreply.github.com>
2023-06-21 16:52:42 -0300

    Modificaciones en imagen galaxia y logo de menu

--- Mas alla de Cielo/js/script.js
+++ Mas alla de Cielo/js/script.js
@@ -2,9 +2,11 @@ import ('style.css');
 import * as THREE from './three.module.js';
 import { OrbitControls } from './orbitcontrols.js';
 
+//________________________________UNIVERSE_______________________________
+
 //Escena y camara
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200000);
 
 //Render
 const renderer = new THREE.WebGLRenderer();
@@ -12,20 +14,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //Luz
-const pointLight = new THREE.PointLight(0xffffff,)
+const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(0,0,0)
 scene.add(pointLight);
 
-//FONDO
-//const fondoStars = new THREE.TextureLoader().load('./img/map-Mercury.jpg');
+//---------------------------------FONDO------------------------------
+//const fondoStars = new THREE.TextureLoader().load('./img/stars.jpg');
 //scene.background = fondoStars;
 
-//------------------SISTEMA SOLAR-----------------
+const geometry = new THREE.SphereGeometry(1000, 60, 40);
+geometry.scale(-1, 1, 1);
 
-//-----------------------------SOL---------------------------
+const textureloader = new THREE.TextureLoader();
+const texture = textureloader.load("img/stars.png");
+const material = new THREE.MeshBasicMaterial({ map: texture});
+
+const sphere = new THREE.Mesh(geometry, material);
+
+//scene.add(sphere);
+//-----------------------------SISTEMA SOLAR---------------------------
+
+//---------------------------------SOL-----------------------------
 const sunTexture = new THREE.TextureLoader().load('img/map-Sun.jpg')
 const sun = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 64, 64),
+    new THREE.SphereGeometry(10, 64, 64),
     new THREE.MeshBasicMaterial( {
         map: sunTexture,
     })
@@ -40,11 +52,11 @@ const earth = new THREE.Mesh(
         map: earthTexture,
     })
 );
-earth.position.set(13, 0, 0);
+earth.position.set(18, 0, 0);
 scene.add(earth);
 
 //orbita Earth
-const orbGeoEarth = new THREE.RingGeometry(12.97, 13, 128);
+const orbGeoEarth = new THREE.RingGeometry(17.99, 18, 128);
 const orbitMaterial = new THREE.LineBasicMaterial({color: 0x00ccff});
 const orbitEarth = new THREE.Line(orbGeoEarth, orbitMaterial);
 
@@ -60,11 +72,11 @@ const mercury = new THREE.Mesh(
         map: mercuryTexture,
     })
 );
-mercury.position.set(-8, 0, 0);
+mercury.position.set(-13, 0, 0);
 scene.add(mercury);
 
 //orbita Mercury
-const orbGeoMerc = new THREE.RingGeometry(7.97, 8, 128);
+const orbGeoMerc = new THREE.RingGeometry(12.99, 13, 128);
 const orbMatMerc = new THREE.LineBasicMaterial({color: 0xffffff});
 const orbitMercury = new THREE.Line(orbGeoMerc, orbMatMerc);
 
@@ -80,11 +92,11 @@ const venus = new THREE.Mesh(
         map: venusTexture,
     })
 );
-venus.position.set(0, 0, -10.5);
+venus.position.set(0, 0, -15.5);
 scene.add(venus);
 
 //orbita Venus
-const orbGeoVen = new THREE.RingGeometry(10.47, 10.5, 128);
+const orbGeoVen = new THREE.RingGeometry(15.49, 15.5, 128);
 const orbMatVen = new THREE.LineBasicMaterial({color: 0xfffc00});
 const orbitVenus = new THREE.Line(orbGeoVen, orbMatVen);
 
@@ -100,11 +112,11 @@ const marte = new THREE.Mesh(
         map: venusTexture,
     })
 );
-marte.position.set(0, 0, 15);
+marte.position.set(0, 0, 20);
 scene.add(marte);
 
 //orbita Marte
-const orbGeoMar = new THREE.RingGeometry(14.97, 15, 128);
+const orbGeoMar = new THREE.RingGeometry(19.99, 20, 128);
 const orbMatMar = new THREE.LineBasicMaterial({color: 0xff0000});
 const orbitMarte = new THREE.Line(orbGeoMar, orbMatMar);
 
@@ -120,11 +132,11 @@ const jupiter = new THREE.Mesh(
         map: jupiterTexture,
     })
 );
-jupiter.position.set(0, 0, 27);
+jupiter.position.set(0, 0, 32);
 scene.add(jupiter);
 
 //orbita Jupiter
-const orbGeoJup = new THREE.RingGeometry(26.97, 27, 128);
+const orbGeoJup = new THREE.RingGeometry(31.99, 32, 128);
 const orbMatJup = new THREE.LineBasicMaterial({color: 0xeefe22});
 const orbitJupiter = new THREE.Line(orbGeoJup, orbMatJup);
 
@@ -140,7 +152,7 @@ const saturno = new THREE.Mesh(
         map: saturnoTexture,
     })
 );
-saturno.position.set(0, 0, -35);
+saturno.position.set(0, 0, -40);
 scene.add(saturno);
 
 //Anillo
@@ -152,13 +164,13 @@ const anillo = new THREE.Mesh(
     })
 );
 anillo.material.side = THREE.DoubleSide;
-anillo.position.set(0, 0, -35);
+anillo.position.set(0, 0, -40);
 anillo.rotateX(90);
 
 scene.add(anillo);
 
 //orbita Saturno
-const orbGeoSat = new THREE.RingGeometry(34.97, 35, 128);
+const orbGeoSat = new THREE.RingGeometry(39.99, 40, 128);
 const orbMatSat = new THREE.LineBasicMaterial({color: 0xddddff});
 const orbitSaturno = new THREE.Line(orbGeoSat, orbMatSat);
 
@@ -174,11 +186,11 @@ const urano = new THREE.Mesh(
         map: uranoTexture,
     })
 );
-urano.position.set(45, 0, 0);
+urano.position.set(50, 0, 0);
 scene.add(urano);
 
 //orbita Urano
-const orbGeoUra = new THREE.RingGeometry(44.97, 45, 128);
+const orbGeoUra = new THREE.RingGeometry(49.99, 50, 128);
 const orbMatUra = new THREE.LineBasicMaterial({color: 0xaabdff});
 const orbitUrano = new THREE.Line(orbGeoUra, orbMatUra);
 
@@ -194,11 +206,11 @@ const neptuno = new THREE.Mesh(
         map: neptunoTexture,
     })
 );
-neptuno.position.set(0, 0, 60);
+neptuno.position.set(0, 0, 65);
 scene.add(neptuno);
 
 //orbita Neptuno
-const orbGeoNep = new THREE.RingGeometry(59.97, 60, 128);
+const orbGeoNep = new THREE.RingGeometry(64.99, 65, 128);
 const orbMatNep = new THREE.LineBasicMaterial({color: 0xabdfff});
 const orbitNeptuno = new THREE.Line(orbGeoNep, orbMatNep);
 
@@ -207,7 +219,7 @@ orbitNeptuno.material.side = THREE.DoubleSide;
 scene.add(orbitNeptuno);
 
 //---------------------------------nebulosa----------------------------
-const textureLoader = new THREE.TextureLoader();
+/*const textureLoader = new THREE.TextureLoader();
 const nebuTexture = textureLoader.load('img/nebulosa.jpeg');
 const nebuMaterial = new THREE.SpriteMaterial({ map: nebuTexture});
 
@@ -216,21 +228,38 @@ const nebulaSprite = new THREE.Sprite(nebuMaterial);
 //nebulaSprite.material.side = THREE.DoubleSide;
 nebulaSprite.scale.set(500, 300,);
 nebulaSprite.position.set(1600, 600, -50);
-scene.add(nebulaSprite);
+scene.add(nebulaSprite);*/
 
-//grilla
+//----------------------------GALAXY----------------------------------
+
+const texLoadGalaxy = new THREE.TextureLoader().load('img/galaxy.png');
+
+const galaxyMaterial = new THREE.MeshBasicMaterial({ map: texLoadGalaxy, transparent: true, side: THREE.DoubleSide});
+const galaxyGeometry = new THREE.PlaneGeometry(200000, 200000, 1);
+const plano = new THREE.Mesh(galaxyGeometry, galaxyMaterial);
+
+//plano.scale.set(200, 200);
+
+plano.position.set(-35000, 34000, 0);
+scene.add(plano);
+
+//---------------------------grilla--------------------
 const grid = new THREE.GridHelper(100,200);
 //scene.add(grid);
 
 //--------------------------Controles Orbitales-------------------
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 1;
-controls.maxDistance = 5000;
+controls.maxDistance = 100000;
 
-camera.position.set (0, 20, 120);
+camera.position.set (0, 30, 120);
 
 //---------------------------Functions-----------------------------
-function addStar() {
+
+
+//--------------------------------STARS----------------------------
+
+/*function addStar() {
     const stargeometry = new THREE.SphereGeometry(0.1, 4, 4);
     const starmaterial = new THREE.MeshBasicMaterial(0xffffff);
     const star = new THREE.Mesh(stargeometry, starmaterial);
@@ -241,17 +270,32 @@ function addStar() {
     scene.add(star);
 }
 
-Array(10000).fill().forEach(addStar);
+Array(10000).fill().forEach(addStar);*/
 
 function animate() {
     requestAnimationFrame( animate );
     mercury.rotation.y += 0.01
     venus.rotation.y += 0.008
-    earth.rotation.y += 0.007
+    earth.rotation.y += 0.01
     marte.rotation.y += 0.004
     jupiter.rotation.y += 0.0008
+
+    /*let time = Date.now() * 0.001; //tiempo actual
+    let position = orbitEarth.getPoint(time);
+    earth.position.set(position.x, 0, position.y);*/
 
     renderer.render(scene, camera);
 }
 animate();
+//_______________________________________________________________________
 
+
+//________________________________Elementos_______________________________
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+    navToggle.classList.toggle("nav-toggle_visible");
+})
